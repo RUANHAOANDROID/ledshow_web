af3680a78dbc74dfc22998e4b1f6280436943d99
hao88 <52927295@qq.com>
2024-02-28 19:07:20 +0800

    重连

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8080";
-const WS_URL = 'ws://localhost:8080/ws';
+const BASE_URL = "http://open.hao88.cloud:6688";
+const WS_URL = 'ws://open.hao88.cloud:6688/ws';
 const CONNECT_TIMEOUT = Duration(seconds: 10000);
 const RECEIVE_TIMEOUT = Duration(seconds: 10000);
 const defaultPadding = 16.0;
